e43bc48ba65fb18b424a8a36ed0bae30a8f46d6d
itggot-edvin-alestig <edvin.alestig@itggot.se>
2018-10-11 14:07:49 +0200

    Added a class for checking word validity

---
+++ words.rb
@@ -0,0 +1,18 @@
+class Words
+    def initialize()
+        # Reads the saved word list
+        wordfile = File.readlines("wordlist.txt")
+
+        # Goes through all the words and removes \n-characters and other whitespace
+        @words = []
+        wordfile.each do |line|
+            # Adds the chomped word to the words array
+            @words << line.chomp
+        end
+    end
+
+    def check_word(word)
+        # Checks if a word is in the words array
+        return @words.include? word.downcase
+    end
+end
