6138b8e24bf8107f1284034c38b50dcbe8db1f1f
itggot-edvin-alestig <edvin.alestig@itggot.se>
2018-10-25 13:11:42 +0200

    Added routes to json from the game

--- webapp/app.rb
+++ webapp/app.rb
@@ -1,15 +1,29 @@
 require("sinatra")
+require("json")
+require_relative("../logic/game.rb")
+game = nil
 
 get("/") do
-    return "<h1>Hello world!</h1>"
+    game = Game.new(2)
+    return "<h1>Creating new game!</h1><p><a href='/p1'>Player 1</a><br><a href='/p2'>Player 2</a></p>"
 end
 
 
 get("/p1") do
-    return "<h1>You are now player 1</h1>"
+    return "<h1>You are now player 1</h1><p><a href='/getp1'>Get data</a></p>"
 end
 
 
 get("/p2") do
-    return "<h1>You are now player 2</h1>"
+    return "<h1>You are now player 2</h1><p><a href='/getp2'>Get data</a></p>"
+end
+
+
+get("/getp1") do
+    return game.dictify(0).to_json
+end
+
+
+get("/getp2") do
+    return game.dictify(1).to_json
 end
