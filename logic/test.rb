3043fce3dd382e4abd502943e30629d00e9a5870
itggot-edvin-alestig <edvin.alestig@itggot.se>
2018-10-25 16:16:31 +0200

    Test for invalid words

--- logic/test.rb
+++ logic/test.rb
@@ -5,6 +5,7 @@ game = Game.new(2)
 game.add_new_letters([{letter: "F", row: 7, col: 7}, {letter: "O", row: 7, col: 8}, {letter: "R", row: 7, col: 9}])
 game.add_new_letters([{letter: "O", row: 8, col: 7}, {letter: "F", row: 8, col: 8}])
 game.add_new_letters([{letter: "T", row: 7, col: 10}, {letter: "S", row: 7, col: 11}])
+game.add_new_letters([{letter: "F", row: 8, col: 10}, {letter: "Q", row: 8, col: 11}, {letter: "T", row: 8, col: 12}])
 
 
 d = game.dictify(0).to_json()
