87116466a2bef2313b5285f953f7bb88a83a8d0a
itggot-edvin-alestig <edvin.alestig@itggot.se>
2018-11-14 15:50:06 +0100

    Updated test.rb

--- logic/test.rb
+++ logic/test.rb
@@ -1,13 +1,24 @@
 require_relative("game.rb")
 
-game = Game.new(2)
+b = false
 
-game.add_new_letters([{letter: "F", row: 7, col: 7}, {letter: "O", row: 7, col: 8}, {letter: "R", row: 7, col: 9}])
-game.add_new_letters([{letter: "O", row: 8, col: 7}, {letter: "F", row: 8, col: 8}])
-game.add_new_letters([{letter: "T", row: 7, col: 10}, {letter: "S", row: 7, col: 11}])
-game.add_new_letters([{letter: "F", row: 8, col: 10}, {letter: "Q", row: 8, col: 11}, {letter: "T", row: 8, col: 12}])
-game.add_new_letters([{letter: {letter: "blank", value: "O"}, row: 9, col: 10}, {letter: "K", row: 9, col: 11}])
+# Loops until at least one addition is valid.
+while !b
+    puts "\n\n--- NEW GAME ---"
+    game = Game.new(2)
+    arr = []
 
+    arr << game.add_new_letters([{letter: "F", row: 7, col: 7}, {letter: "O", row: 7, col: 8}, {letter: "R", row: 7, col: 9}])
+    arr << game.add_new_letters([{letter: "O", row: 8, col: 7}, {letter: "F", row: 8, col: 8}])
+    arr << game.add_new_letters([{letter: "T", row: 7, col: 10}, {letter: "S", row: 7, col: 11}])
+    arr << game.add_new_letters([{letter: "F", row: 8, col: 10}, {letter: "Q", row: 8, col: 11}, {letter: "T", row: 8, col: 12}])
+    arr << game.add_new_letters([{letter: {letter: "blank", value: "O"}, row: 9, col: 10}, {letter: "K", row: 9, col: 11}])
+    arr << game.add_new_letters([{letter: "E", row: 4, col: 7}, {letter: "E", row: 4, col: 8}])
+
+    if arr.include? true
+        b = true
+    end
+end
 
 d = game.dictify(0).to_json()
 # p d
