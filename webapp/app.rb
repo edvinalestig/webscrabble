665757491ee97f0d2d56de9478a3edd9b6fb65ec
itggot-edvin-alestig <edvin.alestig@itggot.se>
2018-11-23 16:21:01 +0100

    Converted the app to a class and added rake

--- webapp/app.rb
+++ webapp/app.rb
@@ -1,57 +1,60 @@
-require("sinatra")
-require("json")
 require_relative("../logic/game.rb")
-game = nil
+require 'json'
+$game = nil
 
-get("/") do
-    game = Game.new(2)
-    return "<h1>Creating new game!</h1><p><a href='/p1'>Player 1</a><br><a href='/p2'>Player 2</a></p>"
-end
+class App < Sinatra::Base
 
 
-get("/p1") do
-    return "<h1>You are now player 1</h1><p><a href='/getp1'>Get data</a></p><script>#{File.read('post.js')}</script>"
-end
+    get("/") do
+        $game = Game.new(2)
+        return "<h1>Creating new game!</h1><p><a href='/p1'>Player 1</a><br><a href='/p2'>Player 2</a></p>"
+    end
 
 
-get("/p2") do
-    return "<h1>You are now player 2</h1><p><a href='/getp2'>Get data</a></p><script>#{File.read('post.js')}</script>"
-end
+    get("/p1") do
+        return "<h1>You are now player 1</h1><p><a href='/getp1'>Get data</a></p><script>#{File.read('webapp/post.js')}</script>"
+    end
 
 
-get("/getp1") do
-    return game.to_hash(0).to_json
-end
+    get("/p2") do
+        return "<h1>You are now player 2</h1><p><a href='/getp2'>Get data</a></p><script>#{File.read('webapp/post.js')}</script>"
+    end
 
 
-get("/getp2") do
-    return game.to_hash(1).to_json
-end
+    get("/getp1") do
+        return $game.to_hash(0).to_json
+    end
 
 
-post("/p1") do
-    p params
-    game.response(params)
-    redirect("/p1")
-end
+    get("/getp2") do
+        return $game.to_hash(1).to_json
+    end
 
 
-post("/p2") do
-    p params
-    game.response(params)
-    redirect("/p2")
-end
+    post("/p1") do
+        p params
+        $game.response(params)
+        redirect("/p1")
+    end
 
 
-post("/testpost") do
-    request.body.rewind
-    p JSON.parse(request.body.read, symbolize_names: true)
-    request.body.rewind
-    game.response(JSON.parse(request.body.read, symbolize_names: true))
-    redirect("/p1")
-end
+    post("/p2") do
+        p params
+        $game.response(params)
+        redirect("/p2")
+    end
 
 
-get("/testpost") do
-    return "<script>#{File.read('post.js')}; sendpost('/testpost', #{File.read('testjson.json')});</script>"
+    post("/testpost") do
+        request.body.rewind
+        p JSON.parse(request.body.read, symbolize_names: true)
+        request.body.rewind
+        $game.response(JSON.parse(request.body.read, symbolize_names: true))
+        redirect("/p1")
+    end
+
+
+    get("/testpost") do
+        return "<script>#{File.read('webapp/post.js')}; sendpost('/testpost', #{File.read('webapp/testjson.json')});</script>"
+    end
 end
