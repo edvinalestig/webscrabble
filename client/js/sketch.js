9dc2f215cf40c863e7aa29e61207754e29985f8b
Ida Franzén Karlsson <ida.franzenkarlsson@itggot.se>
2018-12-02 00:15:10 +0100

    Created p5 canvas

---
+++ client/js/sketch.js
@@ -0,0 +1,6 @@
+function setup() {
+    var canvasDiv = document.getElementById("playfield");
+    var canvas = createCanvas(canvasDiv.offsetWidth, canvasDiv.offsetHeight);
+    canvas.parent("playfield");
+    background(31, 31, 31);
+}
