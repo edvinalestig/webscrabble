656cf7194f19f9d08dd66f3e1d1c57f04b09309f
itggot-edvin-alestig <edvin.alestig@itggot.se>
2019-01-03 17:07:54 +0100

    Added points and a star in the centre

--- client/js/board.js
+++ client/js/board.js
@@ -3,15 +3,42 @@ function drawBoard() {
     const tileLength = 45;
     const upLeftCornerX = 200;
     const upLeftCornerY = 20;
-    stroke(0);
-    fill(0);
 
+    fill(33);
+    rect(upLeftCornerX, upLeftCornerY, tileLength * 15, tileLength * 15);
+
+    stroke(200);
+    fill(200);
 
     for (let i = 0; i <= 15; i++) {
-        console.log(i);
         // Horizontal
         line(upLeftCornerX, upLeftCornerY + i * tileLength, upLeftCornerX + 15 * tileLength, upLeftCornerY + i * tileLength);
         // Vertical
         line(upLeftCornerX + i * tileLength, upLeftCornerY, upLeftCornerX + i * tileLength, upLeftCornerY + 15 * tileLength);
     }
+
+    // Centre tile
+    const x = upLeftCornerX + tileLength * 7.5;
+    const y = upLeftCornerY + tileLength * 7.5;
+    star(x, y, 8, 15, 5);
 }
+
+// Copied from the p5 website with minor changes
+// https://p5js.org/examples/form-star.html
+function star(x, y, radius1, radius2, npoints) {
+    const angle = TWO_PI / npoints;
+    const halfAngle = angle/2.0;
+
+    fill(150, 0, 0);
+    stroke(150, 0, 0);
+    beginShape();
+    for (let a = PI / -2; a < TWO_PI; a += angle) {
+      let sx = x + cos(a) * radius2;
+      let sy = y + sin(a) * radius2;
+      vertex(sx, sy);
+      sx = x + cos(a+halfAngle) * radius1;
+      sy = y + sin(a+halfAngle) * radius1;
+      vertex(sx, sy);
+    }
+    endShape(CLOSE);
+  }
--- client/js/letter.js
+++ client/js/letter.js
@@ -1,6 +1,6 @@
 class LetterRack {
 
-    constructor(letters) {
+    constructor() {
         this.width   = 70;
         this.height  = 70;
         this.letters = [];
@@ -17,32 +17,15 @@ class LetterRack {
             strokeWeight(0);
             let yPos = this.yPos + i * (this.height + 28.5);
             rect(this.xPos, yPos, this.width, this.height, 10);
-            console.log(this.letters[i]);
-            // letter(this.letters[i]);
 
             fill(255);
-            text(this.letters[i], this.xPos + 10, yPos + 20);
+            textSize(48);
+            text(this.letters[i], this.xPos + 18, yPos + 50);
+            textSize(12);
+            p = getPoints(this.letters[i]);
+            text(p, this.xPos + 54, yPos + 19);
         }
-
-
-        // let j = 0;
-        // for (let i = this.yPos; i <= this.height * 9; i += this.height + 28.5) {
-        //     fill(this.colour);
-        //     strokeWeight(0);
-        //     rect(this.xPos, i, this.width, this.height, 10);
-            
-        //     console.log(j);
-        //     //letter();
-        //     j++;
-        // }
         noLoop();
-    }
-    
-    letter(letter_) {
-        console.log("text");
-        fill(255);
-        
-        text(letter_, this.xPos, this.yPos);
     }
     
     manageLetters() {
@@ -50,4 +33,6 @@ class LetterRack {
         this.letters  = gameObject.game.you.rack;
         console.log(this.letters);
     }
+
+
 }
