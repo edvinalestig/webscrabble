e45b4fcd969e1986af6ea9de871ce494497bc835
itggot-edvin-alestig <edvin.alestig@itggot.se>
2019-01-09 18:33:55 +0100

    Moved click parts into own file

--- client/js/sketch.js
+++ client/js/sketch.js
@@ -161,54 +161,7 @@ function mouseClicked() {
     if (x >= playfield.xPos && x <= playfield.xPos + playfield.length) {
         if (y >= playfield.yPos && y <= playfield.yPos + playfield.length) {
             // Click is on the board
-            // Get which tile was clicked on
-            const row = floor((y - playfield.yPos) / playfield.tileLength);
-            const col = floor((x - playfield.xPos) / playfield.tileLength);
-            console.log("Row: ", row, " Col: ", col);
-
-            // Check if the tile has a letter already
-            if (gameObject.game.board.tiles[row][col].letter != null) {
-                return;
-            }
-
-            // Check if there is a letter selected to be placed
-            if (selectedLetter != undefined) {
-                // Check if the letter has already been placed
-                for (let tile of placedTiles) {
-                    if (tile.rack == selectedLetter) {
-                        return;
-                    }
-                    if (tile.row == row && tile.col == col) {
-                        return;
-                    }
-                }
-
-                // Draw the letter
-                const letter = gameObject.game.you.rack[selectedLetter];
-                const o = {
-                    "rack": selectedLetter,
-                    "row": row,
-                    "col": col
-                };
-                placedTiles.push(o);
-                playfield.drawLetter(letter, row, col);
-                letterRack.hide(selectedLetter);
-                letterRack.hidden.push(selectedLetter);
-                selectedLetter = undefined;
-            } else {
-                // Remove the placed tile
-                playfield.removeLetter(row, col);
-                let index;
-                for (i = 0; i < placedTiles.length; i++) {
-                    if (placedTiles[i].row == row && placedTiles[i].col == col) {
-                        index = i;
-                    }
-                }
-                const spliced = placedTiles.splice(index, 1);
-                console.log(spliced[0]);
-                letterRack.hidden.splice(letterRack.hidden.indexOf(spliced[0].rack), 1);
-                letterRack.show();
-            }
+            checkBoard();
         }
     }
 
@@ -216,25 +169,7 @@ function mouseClicked() {
     if (x >= letterRack.xPos && x <= letterRack.xPos + letterRack.width) {
         if (y >= letterRack.yPos && y <= letterRack.yPos + letterRack.height * 7 + 28.5 * 6) {
             // Click is on the rack
-            console.log("rack!");
-
-            let pos = floor((y - letterRack.yPos) / (letterRack.height + 28.5));
-            console.log(pos);
-            if (selectedLetter == pos) {
-                letterRack.deselect(pos);
-                selectedLetter = undefined;
-            } else {
-                for (let tile of placedTiles) {
-                    if (tile.rack == pos) {
-                        return;
-                    }
-                }
-                if (selectedLetter != undefined) {
-                    letterRack.deselect(selectedLetter);
-                }
-                letterRack.select(pos);
-                selectedLetter = pos;                
-            }
+            checkRack();
         }
     }
 }
